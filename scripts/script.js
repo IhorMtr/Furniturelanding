c69a02e7bf6ea829f95ae7ad2d5c395243bdba17
IhorMtr <igormotornyyy@gmail.com>
2024-08-14 16:25:52 +0300

    reworked reviews section (added carousel)

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,35 @@
-$('.owl-carousel').owlCarousel({
+$('#owl-one').owlCarousel({
+    autoplay: false,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: false,
+    nav: false,
+    margin: 0,
+    dots: false,
+    rewind: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1100: {
+            items: 3
+        }
+    },
+});
+
+
+$('#owl-two').owlCarousel({
     autoplay: false,
     autoplayTimeout: 6500,
     autoplaySpeed: 1000,
     loop: false,
     nav: true,
     margin: 0,
-    autoWidth: true,
+    dots: false,
+    rewind: true,
     responsive: {
         0: {
             items: 1
